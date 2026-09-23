8d73570433da86c36d5516f0d4fd7a1952c29cd2
요령 <bgj0127@gmail.com>
2026-09-23 17:31:56 +0900

    Refactor emoji app logic and UI components

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if "result_text" not in st.session_state:
 user_input = st.text_area(
     "입력창",
     label_visibility="collapsed",
-    placeholder="문장을 입력하면 이모지가 듬뿍 들어간 밈으로 변환됩니다! (최대 1000자)",
+    placeholder="문장을 입력하면 이모지를 듬뿍 넣어드립니다! (최대 1000자)",
     height=120, 
     max_chars=1000,
     key="input_text",
